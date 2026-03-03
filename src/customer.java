fff7da6a841bf6d0f6c9b039e9b1f6e69d637096
ravenmancao <venmancao@gmail.com>
2026-03-03 14:33:26 +0800

    commit changes

---
+++ src/customer.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Raven Mancao
+ */
+public class customer {
+    
+  private int customerId;
+  private String name;
+  private String cpNumber;
+  private String address;
+  private String license;
+  
+  
+}
